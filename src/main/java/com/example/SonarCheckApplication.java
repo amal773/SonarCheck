18e7784ba668b63e5ea9f950acb30736d388f8e6
amal773 <amalkuniyilparambath@gmail.com>
2024-05-29 14:43:05 +0530

    first commit

---
+++ src/main/java/com/example/SonarCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarCheckApplication.class, args);
+	}
+
+}
